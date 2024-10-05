b6640daddbbc0fa62d879efee824637e2f3c03ad
bhagat-abhishek <inst.abhishek@gmail.com>
2024-10-06 01:32:33 +0530

    Show Toast

--- src/views/Addjob.vue
+++ src/views/Addjob.vue
@@ -2,6 +2,7 @@
 import router from '@/router';
 import axios from 'axios';
 import { reactive } from 'vue';
+import { useToast } from 'vue-toastification';
 
 const form = reactive({
     type: 'Full-Time',
@@ -17,6 +18,7 @@ const form = reactive({
     }
 });
 
+const toast = useToast();
 
 const handleSubmit = async () => {
     const newJob = {
@@ -35,11 +37,12 @@ const handleSubmit = async () => {
 
     try {
         const response = await axios.post('/api/jobs', newJob);
-        // @todo - show toast
+        toast.success('Job added successfully')
         router.push(`/jobs/${response.data.id}`);
     } catch (error) {
         console.error("Posting Job Failed: ", error);
-        // @todo - show toast
+        toast.error('Job was not added')
+
     }
 
 }
